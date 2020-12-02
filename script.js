b076984afe5a9eaf0d74b5c271e422f7e51513d0
Vagner Felix Bandeira <61794380+Vbandeirarj@users.noreply.github.com>
2020-12-02 18:17:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado Sro(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
